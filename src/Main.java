7bb6049a6e8cd263856159358dc92a5702be0fff
damienbailey <38173154+damienbailey@users.noreply.github.com>
2021-01-16 18:26:22 +1100

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
 
+
+
+
+    public static double grossYearlySalary(double hoursWorked, double wageRate, int vacationDays){
+        double grossWeeklyWage = hoursWorked * wageRate;
+        double deductionVacationDays = vacationDays * 8 * wageRate;
+        double yearlySalary = grossWeeklyWage * 52 - deductionVacationDays;
+        return yearlySalary;
+    }
+
     public static void main(String[] args) {
-        
+
+        double calculatedGrossYearlySalary = grossYearlySalary(38,1, 1);
+        System.out.println(calculatedGrossYearlySalary);
     }
 
 }
